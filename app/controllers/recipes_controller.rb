31e8eb654492877ca62f94e9ceda94a7cf233d53
iloveomelette <mizuki1998hosei@gmail.com>
2022-03-23 23:19:07 +0900

    create,newアクションとビューの記述

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,22 +1,32 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.includes(:user).order(created_at: :desc)
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = current_user.recipes.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe, notice: "投稿しました!"
+    else
+      render :new
+    end
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :content, :menu_image, :cooking_time, :cooking_cost, :calorie)
   end
 end
