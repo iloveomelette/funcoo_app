2002dd6cb5aad7ec7acb862962b5205ee5fd1e2d
iloveomelette <mizuki1998hosei@gmail.com>
2022-03-29 16:35:12 +0900

    ゲストユーザのemail,パスワードを変更不可、再設定送信機能を止める処理を記述

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_normal_user, only: :destroy
+  before_action :ensure_normal_user, only: %i[update destroy]
 
+  # destroy,updateアクションが実行される前にゲストユーザか確認をする処理
   def ensure_normal_user
-    redirect_to recipes_path, alert: "ゲストユーザは削除できません" if resource.email == "guest@example.com"
+    redirect_to recipes_path, alert: "ゲストユーザは更新・削除できません" if resource.email == "guest@example.com"
   end
 end
