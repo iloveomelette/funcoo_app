c7850810c26be68f51f1644cf101eb47e65d21e9
iloveomelette <mizuki1998hosei@gmail.com>
2022-04-10 17:29:53 +0900

    kaminariの導入とビューで表示

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,9 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[edit update destroy]
+  PER_PAGE = 20
 
   def index
-    @recipes = Recipe.includes(:user, :makes).order(created_at: :desc).limit(20)
+    @recipes = Recipe.includes(:user, :makes).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
     # レコメンド機能の呼び出し
     @recommend = Recipe.recommend(current_user) if current_user.characteristic == "general"
   end
