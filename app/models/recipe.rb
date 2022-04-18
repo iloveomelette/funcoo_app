30f9312e20bff23001c1a0080f77d9feaec42324
iloveomelette <mizuki1998hosei@gmail.com>
2022-04-18 22:32:56 +0900

    レコメンドメソッド内の分割と修正

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -18,23 +18,33 @@ class Recipe < ApplicationRecord
     makes.any? { |make| make.user_id == user.id }
   end
 
-  # レコメンド機能
-  def self.recommend(user)
-    # ログインユーザが「作ってみた！」を押した全ての投稿を新着順に取得
-    base_recipes = Make.where(user_id: user.id).order(created_at: :desc)
-    # その投稿の中で最新のものを取得
-    base_recipe = base_recipes.first
-    return nil if base_recipe.nil?
+  class << self
+    # レコメンド機能
+    def recommend(user)
+      # ログインユーザが「作ってみた！」を押した全ての投稿を新着順に取得
+      base_recipes = Make.where(user_id: user.id).order(created_at: :desc)
+      # その投稿の中で5番目のものを取得
+      base_recipe = base_recipes[4]
+      return nil if base_recipe.nil?
 
-    # その投稿に「作ってみた！」を押した全ての他ユーザを取得
-    others = Make.where(recipe_id: base_recipe.recipe_id).order(created_at: :desc)
-    # その投稿の中で最後に押した他ユーザを取得
-    other = others.first
-    # そのユーザが「作ってみた！」を押した全ての投稿を取得
-    other_maked_recipes = Make.where(user_id: other.user_id).order(created_at: :desc)
-    # その投稿の中で最新の投稿を取得する
-    other_maked_recipe = other_maked_recipes.first
-    # レシピクラスで投稿を取得する
-    Recipe.find_by(id: other_maked_recipe.recipe_id)
+      other = Recipe.take_user(base_recipe)
+      Recipe.take_recipe(other)
+    end
+
+    def take_user(base_recipe)
+      # その投稿に「作ってみた！」を押した全ての他ユーザを取得
+      others = Make.where(recipe_id: base_recipe.recipe_id).order(created_at: :desc)
+      # その投稿の中で最後に押した他ユーザを返す
+      others.first
+    end
+
+    def take_recipe(other)
+      # そのユーザが「作ってみた！」を押した全ての投稿を取得
+      other_maked_recipes = Make.where(user_id: other.user_id).order(created_at: :desc)
+      # その投稿の中で最新の投稿を取得する
+      other_maked_recipe = other_maked_recipes.first
+      # レシピクラスで投稿を取得する
+      Recipe.find_by(id: other_maked_recipe.recipe_id)
+    end
   end
 end
