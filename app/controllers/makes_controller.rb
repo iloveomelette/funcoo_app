3d598a46a1441718c13e1bcada298adfa2d9b119
iloveomelette <mizuki1998hosei@gmail.com>
2022-04-24 18:04:48 +0900

    レベル機能のリファクタリング

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -1,15 +1,7 @@
 class MakesController < ApplicationController
   def create
     current_user.makes.create!(recipe_id: params[:recipe_id])
-    current_point = current_user.experience_point
-    adding_point = LevelSetting.calc_adding_point(current_user)
-    current_point += adding_point
-    current_user.update!(experience_point: current_point)
-    next_level = LevelSetting.find_by(passing_level: current_user.level + 1)
-    if next_level.threshold <= current_user.experience_point
-      current_user.level += 1
-      current_user.update!(level: current_user.level)
-    end
+    LevelSetting.calc_level(current_user)
     @recipe = Recipe.find(params[:recipe_id])
   end
 
--- app/models/level_setting.rb
+++ app/models/level_setting.rb
@@ -1,10 +1,31 @@
 class LevelSetting < ApplicationRecord
-  def self.calc_adding_point(user)
-    adding_point = if user.experience_point.zero?
-                     15
-                   else
-                     user.experience_point * 0.2
-                   end
-    adding_point.round
+  class << self
+    def calc_level(user)
+      current_point = user.experience_point
+      adding_point = calc_adding_point(user)
+      update_experience_point(user, current_point, adding_point)
+      update_level(user)
+    end
+
+    def calc_adding_point(user)
+      adding_point = user.experience_point * 0.2
+      adding_point.round
+    end
+
+    def update_experience_point(current_user, current_point, adding_point)
+      current_point += adding_point
+      current_user.update!(experience_point: current_point)
+    end
+
+    def update_level(current_user)
+      next_level = LevelSetting.find_by(passing_level: current_user.level + 1)
+      return unless next_level.threshold <= current_user.experience_point
+
+      current_user.level += 1
+      current_user.update!(level: current_user.level)
+    end
   end
+  private_class_method :calc_adding_point,
+                       :update_experience_point,
+                       :update_level
 end
