fc51db1f9a8cc461f43b519b358d5b11eebde2e3
iloveomelette <mizuki1998hosei@gmail.com>
2022-04-24 16:57:15 +0900

    レベル上げ機能の実装

--- app/models/level_setting.rb
+++ app/models/level_setting.rb
@@ -1,2 +1,10 @@
 class LevelSetting < ApplicationRecord
+  def self.calc_adding_point(user)
+    adding_point = if user.experience_point.zero?
+                     15
+                   else
+                     user.experience_point * 0.2
+                   end
+    adding_point.round
+  end
 end
