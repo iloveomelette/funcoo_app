2eca316f4686e1dd1ecf0dcbc0d0b69a8386ec37
iloveomelette <mizuki1998hosei@gmail.com>
2022-04-28 15:29:26 +0900

    OmniauthCallbacksControllerの作成とルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root "homes#index"
   devise_for :users, controllers: {
     registrations: "users/registrations",
-    passwords: "users/passwords"
+    passwords: "users/passwords",
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
