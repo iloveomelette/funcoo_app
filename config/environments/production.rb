b7c5106831ae1a358cfcb1ebf6bee1bafd1b5a36
iloveomelette <mizuki1998hosei@gmail.com>
2022-04-29 22:07:49 +0900

    Deviseのメール送信機能実装 - 本番環境

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,11 +62,22 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "funcoo_app_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
+  config.action_mailer.default_url_options = { host: "https://fun-coo-app.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "smtp.gmail.com",
+    user_name: Rails.application.credentials.google_email[:google_mail_address],
+    password: Rails.application.credentials.google_email[:google_mailer_password],
+    authentication: "login"
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
