131cd2846899c76f5ea7debf0d9b4c132be17ffc
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-03 11:54:25 +0900

    ログイン、新規登録ページのスタイル修正

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -2,6 +2,9 @@ document.addEventListener('turbolinks:load', () => {
   const flash = document.getElementById('flash-wrapper');
   const close = document.getElementById('close');
 
+  if (!close) {
+    return false;
+  }
   close.addEventListener('click', () => {
     flash.classList.add('hidden');
   });
