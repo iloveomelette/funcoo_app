6e431b07f939ac1c5f6a6ccaf68cfe7532ff0ac8
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-07 17:22:50 +0900

    お気に入り機能の非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   def create
+    @recipe = Recipe.find(params[:recipe_id])
     current_user.favorites.create!(recipe_id: params[:recipe_id])
-    redirect_back(fallback_location: recipes_path)
   end
 
   def destroy
+    @recipe = Recipe.find(params[:recipe_id])
     current_user.favorites.find_by(recipe_id: params[:recipe_id]).destroy!
-    redirect_back(fallback_location: recipes_path)
   end
 end
