b4dfc96008dca5bbe858de9ca7b2455c41fe8904
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-07 14:13:03 +0900

    Favoriteモデル、テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_06_001834) do
+ActiveRecord::Schema.define(version: 2022_05_07_050610) do
+
+  create_table "favorites", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_favorites_on_recipe_id"
+    t.index ["user_id", "recipe_id"], name: "index_favorites_on_user_id_and_recipe_id", unique: true
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "genres", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "recipe_id", null: false
@@ -99,6 +109,8 @@ ActiveRecord::Schema.define(version: 2022_05_06_001834) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "recipes"
+  add_foreign_key "favorites", "users"
   add_foreign_key "genres", "recipes"
   add_foreign_key "makes", "recipes"
   add_foreign_key "makes", "users"
