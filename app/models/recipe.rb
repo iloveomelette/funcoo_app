d20d55eb64aa877eded241ece5bf22bf8fd9597f
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-07 14:21:44 +0900

    バリデーション、アソシエーションの設定

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :makes, dependent: :destroy
   has_many :maked_users, through: :makes, source: :user
+  has_many :favorites, dependent: :destroy
   has_one :genre, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 100 }
