4f5f2c68f5ec2dbf6983eaaed0e03b834ba9b2ea
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-08 09:48:17 +0900

    マイページにお気に入りレシピの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def show
     @recipes = if current_user.characteristic == "general"
-                 Recipe.where(id: current_user.maked_recipes.pluck(:recipe_id)).page(params[:page]).per(PER_PAGE)
+                 Recipe.includes(:user, :makes, :favorites).where(id: current_user.maked_recipes.pluck(:recipe_id)).page(params[:page]).per(PER_PAGE)
                else
-                 Recipe.where(user_id: current_user.id).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+                 Recipe.includes(:user, :makes, :favorites).where(user_id: current_user.id).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
                end
+    recipe_ids = Favorite.includes(:user).where(user_id: current_user.id).order(created_at: :desc).pluck(:recipe_id)
+    @favorite_recipes = Recipe.find(recipe_ids)
   end
 end
