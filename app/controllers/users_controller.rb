296b6cfdb24605d9e3df1912ae1934f5ace75940
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-08 12:47:15 +0900

    N+1問題の解消

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @recipes = take_user_recipes
     recipe_ids = Favorite.includes(:user).where(user_id: current_user.id).order(created_at: :desc).pluck(:recipe_id)
-    @favorite_recipes = Recipe.where(id: recipe_ids).page(params[:page]).per(PER_PAGE)
+    @favorite_recipes = Recipe.includes(:makes, :favorites).where(id: recipe_ids).page(params[:page]).per(PER_PAGE)
   end
 
   private
