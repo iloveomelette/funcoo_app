6bfd126133056590086efe1103b27d26176c1440
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-15 16:40:19 +0900

    レシピ内容の制限文字数の修正

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,7 +7,7 @@ class Recipe < ApplicationRecord
   has_one :genre, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 100 }
-  validates :content, presence: true, length: { maximum: 500 }
+  validates :content, presence: true, length: { maximum: 2000 }
   validates :cooking_time, length: { in: 0..200 }
   validates :cooking_cost, length: { in: 0..10_000 }
   validates :calorie, length: { in: 0..3000 }
