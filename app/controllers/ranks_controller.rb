182a265c71a55c5f009eb04a7738802bacf1fe95
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-23 21:08:28 +0900

    ranksコントローラのマジックナンバーを修正

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,6 +1,7 @@
 class RanksController < ApplicationController
+  TOP_FIVE_RANKINGS = 5
   def maked_ranking
-    recipe_ids = Recipe.order(makes_count: :desc).limit(5).pluck(:id)
+    recipe_ids = Recipe.order(makes_count: :desc).limit(TOP_FIVE_RANKINGS).pluck(:id)
     @high_rank_recipes = Recipe.includes(:makes, :favorites).find(recipe_ids)
   end
 end
