dee68e75695466cffea713a927b02044c80e7177
iloveomelette <mizuki1998hosei@gmail.com>
2022-06-04 17:54:54 +0900

    UsersControllerのリファクタリングとマジックナンバーを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,26 @@ class UsersController < ApplicationController
   def show
     if params[:page] == "mypage"
       @recipes = if current_user.characteristic == "general"
-                   Recipe.includes(:user, :makes,
-                                   :favorites).where(id: current_user.maked_recipe_ids).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+                   take_stored_recipe(current_user.maked_recipe_ids)
                  else
-                   Recipe.includes(:user, :makes, :favorites).where(user_id: current_user.id).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+                   take_contributed_recipes(current_user.id)
                  end
-      @favorite_recipes = Recipe.includes(:user, :makes,
-                                          :favorites).where(id: current_user.favorited_recipe_ids).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+      @favorite_recipes = take_stored_recipe(current_user.favorited_recipe_ids)
     else
       @contributor = User.find(params[:id])
-      @contributor_recipes = Recipe.includes(:user, :makes,
-                                             :favorites).where(user_id: @contributor.id).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+      @contributor_recipes = take_contributed_recipes(@contributor.id)
     end
   end
 
   private
+
+  def take_stored_recipe(recipe_ids)
+    Recipe.includes(:user, :makes, :favorites).where(id: recipe_ids).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+  end
+
+  def take_contributed_recipes(user_id)
+    Recipe.includes(:user, :makes, :favorites).where(user_id:).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+  end
 
   def record_not_found
     redirect_to recipes_path
--- app/models/level_setting.rb
+++ app/models/level_setting.rb
@@ -1,4 +1,7 @@
 class LevelSetting < ApplicationRecord
+  FIRST_ADDING_POINT = 15
+  ADDING_POINT_RATE = 0.2
+
   class << self
     def calc_level(user)
       current_point, adding_point = calc_adding_point(user)    # ===== 現在の経験値と加算する経験値を計算する処理 =====
@@ -9,7 +12,7 @@ class LevelSetting < ApplicationRecord
     # ===== ここからclass_privateメソッド =====
     def calc_adding_point(user)
       current_point = user.experience_point
-      adding_point = current_point.zero? ? 15 : user.experience_point * 0.2
+      adding_point = current_point.zero? ? FIRST_ADDING_POINT : user.experience_point * ADDING_POINT_RATE
       [current_point, adding_point.round]
     end
 
